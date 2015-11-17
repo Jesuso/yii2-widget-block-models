e6e0bb96f26b259007616d6b85dc641faa0adb7b
Jesuso <jesuso.1410@gmail.com>
2015-11-17 12:50:03 -0600

    Initial commit

---
+++ BlockModels.php
@@ -0,0 +1,27 @@
+<?php
+namespace jesuso\blockmodels;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+
+class BlockModels extends \yii\base\Widget
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+      return Html::encode
+    }
+
+    /**
+     * Registers the needed assets
+     */
+    public function registerAssets()
+    {
+        parent::registerAssets();
+    }
+}
